40696dd2601c610a27e012477e8f2ad07cad98b0
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-16 13:45:17 -0500

    task 1 started, push  header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import { Provider } from "react-redux";
 import { store } from '../store';
+import Header from './Header';
 
 
 function App() {
-    return <Provider store={store}>Hello Atlas</Provider>
+    return <Provider store={store}>
+      <Header />
+    </Provider>;
 }
 
 export default App;
