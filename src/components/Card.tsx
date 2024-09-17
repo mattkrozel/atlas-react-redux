1024637e68c3db4373ebf226a1b8778de36a725a
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-17 13:26:12 -0500

    task 1, added card & deletecard

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import DeleteCardButton from './DeleteCardButton';
+
+interface cardProps {
+    title: string;
+    description: string;
+}
+
+const Card: React.FC<cardProps> = ({ title, description }) => {
+    return (
+        <div className="card group/card m-3 flex min-h-24 w-full flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue">
+            <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">
+              <span>{title}</span>
+              <DeleteCardButton />
+            </h5>
+            <p className="mt-0 text-left">
+              {description}
+            </p>
+        </div>
+    );
+};
+
+export default Card;
