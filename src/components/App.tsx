162d4ea862a00e3d94c8ceb24d93bc0a5f606d84
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-17 12:59:04 -0500

    task 1, finished footer & deletelistbutt

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import { Provider } from "react-redux";
 import { store } from '../store';
 import Header from './Header';
+import Footer from "./Footer";
+
 
 
 function App() {
     return <Provider store={store}>
       <Header />
+      <Footer />
     </Provider>;
 }
 
