ce07005cb906554a0c21e4b383d58fe91dcfb960
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-19 11:47:08 -0500

    task 1, added newcardform, started list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,20 @@ import { Provider } from "react-redux";
 import { store } from '../store';
 import Header from './Header';
 import Footer from "./Footer";
+import Card from "./Card";
+import NewCardForm from "./NewCardForm";
 
 
 
 function App() {
     return <Provider store={store}>
       <Header />
+      <div>
+        <Card title='card' description="card descript" />
+      </div>
+      <div>
+        <NewCardForm />
+      </div>
       <Footer />
     </Provider>;
 }
