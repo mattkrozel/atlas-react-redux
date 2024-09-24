f3f428d974cbf97403b8e298fc7300562dc3dd3e
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-24 15:15:29 -0500

    task 2 push, added slice and reducers

---
+++ src/slices/listsSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from 'uuid';
+
+
+interface List {
+    id: string;
+    title: string;
+    cardIds: string[];
+}
+
+interface ListsState {
+    lists: List[];
+}
+
+const initialState: ListsState = {
+    lists: [],
+};
+
+const listsSlice = createSlice({
+    name: 'lists',
+    initialState,
+    reducers: {
+        addList: (state, action: PayloadAction<string>) => {
+            const newList: List = {
+                id: uuidv4,
+                title: action.payload,
+                cardIds: [],
+            };
+            state.lists.push(newList);
+        },
+        deleteList: (state, action: PayloadAction<string>) => {
+            state.lists = state.lists.filter((list) => list.id !== action.payload);
+        },
+        addCardToList: (state, action: PayloadAction<{ listId: string; cardId: string }>) => {
+            const { listId, cardId } = action.payload;
+            const list = state.lists.find((list) => list.id === listId);
+            if (list) {
+                list.cardIds.push(cardId);
+            }
+        },
+        clearBoard: (state) => {
+            state.lists = [];
+        },
+    },
+});
+
+export const { addList, deleteList, addCardToList, clearBoard } = listsSlice.actions;
+
+export default listsSlice.reducer;
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import listsReducer from './slices/listsSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        lists: listsReducer,
+    },
 });
 //these types helpful for typescript autocomplete
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 //adds types to dispatch and selector hooks
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
